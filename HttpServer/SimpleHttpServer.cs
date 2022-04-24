0e2c29e7ddac84c0578666bfc1cfea5ee09ae319
Edward Junior <etdj@aluno.ifnmg.edu.br>
2022-04-23 22:46:35 -0300

    Added HttpSeverProject;

---
+++ HttpServer/SimpleHttpServer.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System.Net;
+using System.Text;
+
+namespace HttpServer;
+
+public class SimpleHttpServer : IDisposable
+{
+    private readonly HttpListener _listener = new();
+
+    public SimpleHttpServer()
+    {
+        ListenAsync();
+    }
+
+    public void Dispose()
+    {
+        _listener.Close();
+    }
+
+    private async Task ListenAsync()
+    {
+        _listener.Prefixes.Add("http://localhost:51111/MyApp/"); // Listen on
+        _listener.Start(); // port 51111
+// Await a client request:
+        var context = await _listener.GetContextAsync().ConfigureAwait(false);
+// Respond to the request:
+        var msg = "You asked for: " + context.Request.RawUrl;
+        context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
+        context.Response.StatusCode = (int) HttpStatusCode.OK;
+        using var s = context.Response.OutputStream;
+        using var writer = new StreamWriter(s);
+        await writer.WriteAsync(msg).ConfigureAwait(false);
+    }
+}
